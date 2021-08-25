16bbd7a2e16b205298967fbe936334bc4de2b3b5
Joke Heyndels <jo_hannah@outlook.com>
2021-08-25 13:36:54 +0000

    Create cheatsheet for ES7 React Snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,57 @@
+// // create and export functional component: rfce
+// import React from 'react'
+
+// function FunctionalComponent() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// export default FunctionalComponent
+
+// // create and export functional component as arrow function: rafce
+// import React from 'react'
+
+// const ArrowFunctionComponent = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// export default ArrowFunctionComponent
+
+// // create and export class component: rce
+// import React, { Component } from 'react'
+
+// export class ClassComponent extends Component {
+//     // create constructor: rconst
+//     constructor(props) {
+//         super(props)
+    
+//         this.state = {
+             
+//         }
+//     }
+    
+
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
+
+// export default ClassComponent
+
+
+// // console.log anything: clg
+// console.log(object)
+
+// // console.log a variable with its name:
+// console.log(`variable`, variable)
